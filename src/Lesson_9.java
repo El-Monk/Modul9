95d901db16168f988723b0f84eeed4f421426839
Monk <elmonk.drb@yandex.ru>
2021-05-25 21:33:16 +0300

    ispravlenie koda

--- src/Lesson_9.java
+++ src/Lesson_9.java
@@ -1,13 +1,13 @@
-import com.sun.jdi.event.StepEvent;
-
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Lesson_9 {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("Test9.txt");
-        Scanner scanner = new Scanner(file);
+//        File file = new File("Test9.txt");
+//        Scanner scanner = new Scanner(file);
+        Scanner scanner = new Scanner(new FileInputStream("Test9.txt"));
         String line = scanner.nextLine();
         String[] numbers = line.split(" ");
         int sum = 0;
@@ -16,5 +16,6 @@ public class Lesson_9 {
             sum += Integer.parseInt(number);
         }
         System.out.println(sum);
+        scanner.close();
     }
 }
